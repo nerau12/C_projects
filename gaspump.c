2d7071eff2807bd6a44afac145057d181f1e0b90
nerau12 <jljersin@comcast.net>
2021-08-23 14:50:47 -0600

    new function

--- gaspump.c
+++ gaspump.c
@@ -3,16 +3,22 @@
 
 void print_menu();
 float get_gallons_bought();
+void print_reciept(float gallons_bought);
 
 const int MAX_GAS = 25;
 const float PRICE_OF_GAS = 2.30;
 
 int main(int argc, char *argv[]){
+    float gallons_bought;
+
     print_menu();
+    gallons_bought = get_gallons_bought();
+
+    return 0;
 }
 
 void print_menu(){
-    printf('Test');
+    
 }
 
 float get_gallons_bought(){
@@ -21,4 +27,8 @@ float get_gallons_bought(){
     gallons = rand() % MAX_GAS;
 
     return gallons;
+}
+
+void print_reciept(float gallons_bought){
+    
 }
